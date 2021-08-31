974e9a1a769344096ced418bb01e6a765e7a6811
nwaose victor <nwaosevictor1234@gmail.com>
2021-08-31 20:08:21 +0100

    javascript search input

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const countriesEl = document.getElementById("countries");
-const toggleBtn = document.getElementById("toggle")
-const filter = document.getElementById("filter")
-const search = document.getElementById("input")
+const toggleBtn = document.getElementById("toggle");
+const filter = document.getElementById("filter");
+const regionFilter = filter.querySelectorAll("li")
+const search = document.getElementById("input");
 
 async function getCountries() {
     const res = await fetch("https://restcountries.eu/rest/v2/all");
@@ -21,14 +22,14 @@ function displayCountries(countries) {
            <img src=${country.flag} alt="">
         </div>
         <div class="card-body">
-            <h2>${country.name}</h2>
+            <h2 class="country-name">${country.name}</h2>
             <p>
                  <strong>Population:</strong>
                  ${country.population}
             </p>
-            <p><strong>
+            <p class="country-region"><strong>
                 Region:
-            </strong>
+            </strong >
              ${country.region}
            </p>
            <p><strong>Capital:</strong>${country.capital}</p>
@@ -48,8 +49,31 @@ filter.addEventListener("click", ()=> {
 });
 
 
-search.addEventListener("click",(e)=>{
-   const val= e.target.value
+search.addEventListener("input", e =>  {
+   const {value}= e.target;
 
-   console.log(val);
+   const countryName = document.querySelectorAll(".country-name");
+   countryName?.forEach(name =>{ 
+        console.log(name.innerText)
+       if (name.innerText.toLowerCase().includes(value.toLowerCase())){
+            name.parentElement.parentElement.style.display ="block";
+       }else{
+        name.parentElement.parentElement.style.display="none"
+       }
+     
+   })
 })
+regionFilter?.forEach(filter => {
+    filter.addEventListener("click", ()=> {
+        const countryRegion = document.querySelectorAll(".country-region");
+   countryRegion?.forEach(region=>{ 
+        console.log(region.innerText)
+       if (region.innerText.toLowerCase().includes(filter.innerText.toLowerCase())){
+            region.parentElement.parentElement.style.display ="block";
+       }else{
+        region.parentElement.parentElement.style.display="none"
+       }
+     
+   })
+    });
+});

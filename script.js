e034721cd895b4ece104023aa9fc0644e29b7069
nwaose victor <nwaosevictor1234@gmail.com>
2021-09-01 15:00:22 +0100

    Country Modal

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const toggleBtn = document.getElementById("toggle");
 const filter = document.getElementById("filter");
 const regionFilter = filter.querySelectorAll("li")
 const search = document.getElementById("input");
+const modal = document.getElementById("modal");
+const closeBtn = document.getElementById("btn-close");
+
 
 async function getCountries() {
     const res = await fetch("https://restcountries.eu/rest/v2/all");
@@ -38,12 +41,66 @@ function displayCountries(countries) {
        </select>
         `;
         countriesEl.appendChild(countryEl);
+        countryEl.addEventListener("click", ()=>{
+            modal.style.display="flex";
+            showCountryDetails(country);
+        })
+        countriesEl.appendChild(countryEl);
     })
     
+}
+
+
+ 
+function showCountryDetails(country) {
+  const modalBody=modal.querySelector(".modal-body");
+  const modalImg= modal.querySelector("img")
+  
+  modalImg.src= country.flag
+  
+  modalBody.innerHTML= `
+            <h2 class="country-name">${country.name}</h2>
+            <p>
+            <strong>Population:</strong>
+            ${country.population}
+            </p>                                          
+            <p class="country-region"><strong>
+            Region:
+            </strong >
+          ${country.region}
+          </p>
+          <p class="country-region"><strong>
+          Subregion:
+          </strong >
+        ${country.subregion}
+        </p>
+        <p>
+            <strong>Capital:</strong>
+            ${country.capital}
+        </p>
+        <p>
+        <strong>Top Level Domain:</strong>
+        ${country.topLevelDomain[0]}
+    </p>
+    <p>
+    <strong>Currency</strong>
+    ${country.currencies?.map(currency=>currency.code)}
+</p>
+<p>
+<strong>Language</strong>
+${country.languages?.map(language=> language.name)}
+</p>
+    `
 }
 toggleBtn.addEventListener("click", ()=>{
     document.body.classList.toggle("dark");
 })
+
+
+closeBtn.addEventListener("click", ()=>{
+    modal.style.display = "none";
+})
+
 filter.addEventListener("click", ()=> {
     filter.classList.toggle("open");
 });
